8760afa40c3a961d554a7482dc4460cf5cd51167
José Emilio Andrade Izquierdo <jose.andrade.izquierdo@udla.edu.ec>
2023-01-17 00:20:12 -0500

    Cambio7

--- Views/BurgerItemPage.xaml.cs
+++ Views/BurgerItemPage.xaml.cs
@@ -1,9 +1,61 @@
+using BurgerV2JEAI.Models;
+using BurgerV2JEAI.Views;
+
 namespace BurgerV2JEAI.Views;
+
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
 
 public partial class BurgerItemPage : ContentPage
 {
-	public BurgerItemPage()
+    Burger Item = new Burger();
+    bool _flag;
+    public BurgerItemPage()
 	{
 		InitializeComponent();
-	}
+        LoadBurguer();
+    }
+
+    public int ItemId
+    {
+        set { LoadBurguer(value); }
+    }
+
+    public void LoadBurguer(int value = -1)
+    {
+        if (value > -1)
+        {
+            Item = App.BurgerRepo.GetBurger(value);
+            SaveButton.Text = "Editar";
+        }
+
+        BindingContext = Item;
+
+    }
+
+    private void Check_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        _flag = e.Value;
+    }
+
+    private void OnSavedClicked(object sender, EventArgs e)
+    {
+        Item.Name = NameB.Text;
+        Item.Description = DescB.Text;
+        Item.WithExtraCheese = _flag;
+        if (SaveButton.Text == "Editar")
+        {
+            App.BurgerRepo.UpadateBurger(Item);
+        }
+        else
+        {
+            App.BurgerRepo.AddNewBurger(Item);
+        }
+        Shell.Current.GoToAsync("..");
+    }
+
+    private void OnCancelClicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("..");
+    }
+
 }
