c745e208383de39d6c6138cb9ed63df6b6ce6b15
José Emilio Andrade Izquierdo <jose.andrade.izquierdo@udla.edu.ec>
2023-01-17 00:15:11 -0500

    BITP1

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -1,9 +1,10 @@
-﻿using SQLite;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+
 
 namespace BurgerV2JEAI.Models
 {
